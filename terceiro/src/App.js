c060bdbc8fe642646b35782f5099d264d82a3815
GUILHERME ARAUJO FRANCA <guilherme.afranca1@senacsp.edu.br>
2022-05-12 15:32:55 -0300

    criação do projeto terceiroe execução do projeto

--- terceiro/src/App.js
+++ terceiro/src/App.js
@@ -1,20 +1,16 @@
+import { useState } from "react";
+
 function App() {
-  let valor1 = Math.round(Math.random() * 15);
-  let valor2 = Math.round(Math.random() * 15);
+  const [numero, setNumero] = useState(1);
+
   return (
     <div>
-      <p>A soma de 50 e 10 é 50+10={50 + 10}</p>
-      <p>O resto da divisão entre 50 e 3 é{50 % 3}</p>
+      <p>O Numero atual é {numero}</p>
       <p>
-        a soma entre {valor1} e {valor2} é {valor1 + valor2}
+        <button title="Clique" onClick={() => setNumero(numero + 1)}>
+          clique aqui
+        </button>
       </p>
-      <p>
-        O Resultado da soma acima é um numero{" "}
-        {(valor1 + valor2) % 2 === 0 ? " par" : " impar"}{" "}
-      </p>
-      {/* estrutura pernaria 
-      é que utiliza 3 partes!!   
-      {(valor1 + valor2) %2 == 0 ? "par" :"impar"}*/}
     </div>
   );
 }
