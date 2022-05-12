ab991286bd93a9745ca90ee60b4020b1eb6e28bc
GUILHERME ARAUJO FRANCA <guilherme.afranca1@senacsp.edu.br>
2022-05-12 17:36:35 -0300

    edição e execução do terceiro projeto

--- terceiro/src/App.js
+++ terceiro/src/App.js
@@ -1,48 +1,41 @@
 import { useState } from "react";
-
 function App() {
-  const [produtos, setProdutos] = useState([
-    {
-      id: 500,
-      nome: "Mouse",
-      categoria: "Informática",
-      preco: 50.5,
-    },
-    {
-      id: 501,
-      nome: "Teclado",
-      categoria: "Informática",
-      preco: 150.9,
-    },
+  const [produto, setProduto] = useState([
+    { id: 500, nome: "Mouse", categoria: "informática", preco: 50.5 },
+    { id: 501, nome: "Teclado", categoria: "informática", preco: 150.9 },
   ]);
+
   return (
     <div>
       <button
-        onClick={() =>
-          setProdutos([
-            {
+        onClick={() => {
+          let id = prompt("Entre com o id do produto");
+          let nome = prompt("Entre com o nome do produto");
+          let categoria = prompt("Entre com a categoria do produto");
+          let preco = prompt("Entre com o preco do produto");
+          setProduto(
+            produto.concat({
               id: 502,
-              nome: "smartphone",
-              categoria: "telefonia",
+              nome: "SmartFone",
+              categoria: "Telefonia",
               preco: 5000,
-            },
-          ])
-        }
+            })
+          );
+        }}
       >
-        Adicionar produtos
+        Adicionar produto
       </button>
-      <h2>produtos cadastrados</h2>
-      {produtos.map((item, index) => (
+      <h2>Produtos cadastrados</h2>
+      {produto.map((item, index) => (
         <div key={index}>
           <h3>{item.nome}</h3>
           <ul>
-            <li>código do produto {item.id}</li>
-            <li>categoria do produto{item.categoria}</li>
-            <li>preço do produto{item.preco}</li>
+            <li>Código do produto {item.id}</li>
+            <li>Categoria do produto {item.categoria}</li>
+            <li>Preço dp produto {item.preco}</li>
           </ul>
         </div>
       ))}
-      ;
     </div>
   );
 }
