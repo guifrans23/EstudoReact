fde268ba728666a6af4f98d280db89f1786efba9
GUILHERME ARAUJO FRANCA <guilherme.afranca1@senacsp.edu.br>
2022-05-17 17:25:43 -0300

    criação de api em servidor backend para a aplicação em frontend

--- quarto/src/components/main/Container.js
+++ quarto/src/components/main/Container.js
@@ -1,11 +1,37 @@
+import { useState, useEffect } from "react";
+
 import Conteudo from "./Conteudo";
 import Mensagem from "./Mensagem";
 
 export default function Container() {
+  //vamos o estado inicial dos produtos,ou seja,
+  // quais dados existem dentro do objetos produtos.
+  const [produtos, setProduto] = useState([
+    {
+      id: "",
+      nome: "",
+      descricao: "",
+      categoria: "",
+      preco: "",
+      msg: [],
+    },
+  ]);
+
+  useEffect(() => {
+    fetch("http://10.26.49.47:4000/api/services/produtos/listar")
+      .then((response) => response.json())
+      .then((dados) => {
+        setProduto(dados.output);
+      })
+      .catch((erro) =>
+        console.error(`erro ao tentar carregar a api -> ${erro}`)
+      );
+  }, []);
+
   return (
     <div className="Container">
       <Mensagem />
-      <Conteudo />
+      <Conteudo dados={produtos} />
     </div>
   );
 }
--- quarto/src/components/main/Conteudo.js
+++ quarto/src/components/main/Conteudo.js
@@ -1,8 +1,17 @@
-export default function Conteudo() {
+export default function Conteudo(props) {
   return (
     <div className="Conteudo">
-      <h2>Conteudo do site</h2>
-      <p>texto do site</p>
+      <h2>produtos</h2>
+      <div className="produtos">
+        {props.dados.map((item, ix) => (
+          <div key={ix}>
+            <h3>{item.Mome}</h3>
+            <p>{item.Descricao}</p>
+            <p>{item.categoria}</p>
+            <p className="preco">R$ {item.preco}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
